d31c9f65fbf6e4f3c3eb2bc769c372e03948dc3d
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2020-02-27 13:29:52 +0300

    Update Y_CNN.py

--- Artificial_Neural_Network/Y_Network/Y_CNN.py
+++ Artificial_Neural_Network/Y_Network/Y_CNN.py
@@ -1,3 +1,7 @@
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
 import numpy as np
 from keras.layers import Dense, Dropout, Input
 from keras.layers import Conv2D, MaxPooling2D, Flatten
@@ -25,51 +29,52 @@ kernel_size = 3
 dropout = 0.4
 n_filters = 32
 
-# left branch of Y network
-left_inputs = Input(shape=input_shape)
-x = left_inputs
-filters = n_filters
+# build model
+def build_model():
 
-# 3 layers of Conv2D-Dropout-MaxPooling2D
-# number of filters doubles after each layer (32-64-128)
-for i in range(3):
-    x = Conv2D(filters=filters,
-               kernel_size=kernel_size,
-               padding='same',
-               activation='relu')(x)
-    x = Dropout(dropout)(x)
-    x = MaxPooling2D()(x)
-    filters *= 2
+	# left branch of Y network
+	left_inputs = Input(shape=input_shape)
+	x = left_inputs
+	filters = n_filters
+
+	# 3 layers of Conv2D-Dropout-MaxPooling2D
+	# number of filters doubles after each layer (32-64-128)
+	for i in range(3):
+		x = Conv2D(filters=filters,
+		           kernel_size=kernel_size, padding='same',
+		           activation='relu')(x)
+		x = Dropout(dropout)(x)
+		x = MaxPooling2D()(x)
+		filters *= 2
     
-# right branch of Y network
-right_inputs = Input(shape=input_shape)
-y = right_inputs
-filters = n_filters
+	# right branch of Y network
+	right_inputs = Input(shape=input_shape)
+	y = right_inputs
+	filters = n_filters
 
-# 3 layers of Conv2D-Dropout-MaxPooling2D
-# number of filters doubles after each layer (32-64-128)
-for i in range(3):
-    y = Conv2D(filters=filters,
-               kernel_size=kernel_size,
-               padding='same',
-               activation='relu',
-               dilation_rate=2)(y)
-    y = Dropout(dropout)(y)
-    y = MaxPooling2D()(y)
-    filters *= 2
+	# 3 layers of Conv2D-Dropout-MaxPooling2D
+	# number of filters doubles after each layer (32-64-128)
+	for i in range(3):
+		y = Conv2D(filters=filters, kernel_size=kernel_size,
+		           padding='same', activation='relu',
+		           dilation_rate=2)(y)
+		y = Dropout(dropout)(y)
+		y = MaxPooling2D()(y)
+		filters *= 2
 
-# merge left and right branches outputs
-y = concatenate([x,y])
+	# merge left and right branches outputs
+	y = concatenate([x,y])
 
-# feature maps to vector before connecting to dense layer
-y = Flatten()(y)
-y = Dropout(dropout)(y)
-outputs = Dense(num_labels,activation='softmax')(y)
+	# feature maps to vector before connecting to dense layer
+	y = Flatten()(y)
+	y = Dropout(dropout)(y)
+	outputs = Dense(num_labels,activation='softmax')(y)
 
-# build the model in functional API
-model = Model([left_inputs,right_inputs],outputs)
+	# build the model in functional API
+	return Model([left_inputs,right_inputs],outputs)
 
 # verify the model
+model = build_model()
 model.summary()
 
 # compile and train  the model
