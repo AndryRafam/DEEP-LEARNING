fad76eed74ffe6791acea42f1376c0c9ec84b249
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2020-03-09 20:56:20 +0300

    Update newswires.py

--- Classification/newswires.py
+++ Classification/newswires.py
@@ -1,12 +1,17 @@
-# Classifying newswires: Mutliclass classification
+# Reuters newswire classification - 80.5 % accuracy over 5 epochs
+
+from __future__ import print_function
+from __future__ import absolute_import
 
 from keras.datasets import reuters
-import numpy as np
 from keras.models import Model
-from keras.layers import Dense, Input
+from keras.layers import Dense, Input, Activation
+from keras.optimizers import RMSprop
 from keras.layers.merge import concatenate
-import matplotlib.pyplot as plt
 
+import numpy as np
+
+### downloading the data
 (train_data,train_labels), (test_data,test_labels) = reuters.load_data(num_words=10000)
 
 
@@ -25,34 +30,38 @@ y_test = np.array(test_labels)
 
 
 ### network parameters
-hidden_units = 64
-epochs = 9
-batch_size = 512
+hidden_units = 512
+epochs = 5
+batch_size = 32
 input_shape = (10000,)
 num_labels = len(np.unique(y_train))
 
-### Model definition
-left_inputs = Input(shape=input_shape)
-x = Dense(hidden_units,activation='relu')(left_inputs)
-x = Dense(hidden_units,activation='relu')(x)
+### build the model
+def build_model():
+	left_inputs = Input(shape=input_shape)
+	x = left_inputs
+	for i in range(2):
+		x = Dense(hidden_units)(x)
+		x = Activation('relu')(x)
+	
+	right_inputs = Input(shape=input_shape)
+	y = right_inputs
+	for i in range(2):
+		y = Dense(hidden_units)(y)
+		y = Activation('relu')(y)
+	
+	z = concatenate([x,y])
+	outputs = Dense(num_labels)(z)
+	outputs = Activation('softmax')(outputs)
+	return Model([left_inputs,right_inputs],outputs)
 
-right_inputs = Input(shape=input_shape)
-y = Dense(hidden_units,activation='relu')(right_inputs)
-y = Dense(hidden_units,activation='relu')(y)
-
-z = concatenate([x,y])
-
-outputs = Dense(num_labels,activation=('softmax'))(z)
-
-model = Model([left_inputs,right_inputs],outputs)
-
-### Compiling the model
-model.compile(optimizer='rmsprop',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+### Compiling and training the model
+model = build_model()
+optimizer = RMSprop(lr=0.0001)
+model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy',
+	      metrics=['accuracy'])
 
 
-### Validating the approach
 x_val = x_train[:1000]
 partial_x_train = x_train[1000:]
 
@@ -60,48 +69,11 @@ y_val = y_train[:1000]
 partial_y_train = y_train[1000:]
 
 
-### Training the model
 history = model.fit([partial_x_train,partial_x_train],
                     partial_y_train,
                     batch_size=batch_size,
                     validation_data=([x_val,x_val],y_val),
                     epochs=epochs)
 
-
-
-### Plotting the training and validation loss
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-
-epochs = range(1,len(loss)+1)
-
-plt.plot(epochs,loss,label='Training loss')
-plt.plot(epochs,val_loss,label='Validation loss')
-plt.title('Training and validation loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-
-plt.show()
-
-
-
-### Plotting the training and validation accuracy
-plt.clf()
-
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
-
-plt.plot(epochs,acc,label='Training acc')
-plt.plot(epochs,val_acc,label='Validation acc')
-plt.title('Training and validation accuracy')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-
-plt.show()
-
 score = model.evaluate([x_test,x_test],y_test)
 print("\nScore: %.1f%%" % (100.0*score[1]))
-
-
