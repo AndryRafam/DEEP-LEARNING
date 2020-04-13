8b301bd0e69870227799231a35d1b96eb16135c8
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2020-04-13 15:05:47 +0300

    Update MLP.py

--- Artificial_Neural_Network/MLP.py
+++ Artificial_Neural_Network/MLP.py
@@ -1,77 +1,68 @@
-""" MLP on fashion_MNIST with l2 regularizer - 88,7% accuracy over 50 epochs """
+""" MLP (Multi Layer Perceptron) on fashion MNIST with l2 regularizer - 88,7% accuracy over 50 epochs """
 
-### import libraries
+# import libraries
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
+import tensorflow
 import numpy as np
-from keras.layers import Dense, Activation
-from keras.layers import Input
-from keras.models import Model
-from keras.datasets import fashion_mnist
-from keras.regularizers import l2
 import matplotlib.pyplot as plt
 
-### load data
-(x_train,y_train), (x_test,y_test) = fashion_mnist.load_data()
-
-### computer the number of labels
-num_labels = len(np.unique(y_train))
-
-### sample and plot 25 mnist digits from train dataset
-indexes = np.random.randint(0,x_train.shape[0], size=25)
-images = x_train[indexes]
-labels = y_train[indexes]
-
-plt.figure(figsize=(5,5))
-for i in range(len(indexes)):
-    plt.subplot(5,5,i+1)
-    image = images[i]
-    plt.imshow(image,cmap='gray')
-    plt.axis('off')
-
-plt.show()
-plt.close('all')
+from tensorflow.keras.layers import Dense, Activation, Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.regularizers import l2
 
 
-### image dimensions (assumed square)
-image_size = x_train.shape[1]
-input_shape = image_size*image_size
+class Init(object):
+    def __init__(self): # constructor
+        #load_data
+        (self.x_train,self.y_train), (self.x_test,self.y_test) = fashion_mnist.load_data()
 
-### resize and normalize
-x_train = np.reshape(x_train, [-1, input_shape])
-x_train = x_train.astype('float32')/255
-x_test = np.reshape(x_test, [-1, input_shape])
-x_test = x_test.astype('float32')/255
+        # compute the number of labels
+        self.num_labels = len(np.unique(self.y_train))
 
-### network parameters
-batch_size = 128
-hidden_units = 256
-kernel_regul = l2(0.0001)
-epochs=50
+        # image dimensions (assumed square)
+        self.image_size = self.x_train.shape[1]
+        self.input_shape = self.image_size*self.image_size
 
-### 3-layer MLP model with relu and l2 regularizer after each layer
-def build_model():
-	inputs = Input(shape=(input_shape,))
-	for i in range(2):
-		y = inputs
-		y = Dense(hidden_units,kernel_regularizer=kernel_regul)(y)
-		y = Activation('relu')(y)
-		
-	y = Dense(num_labels)(y)
-	outputs = Activation('softmax')(y)
-	return Model(inputs,outputs)
+        # resize and normalize
+        self.x_train = np.reshape(self.x_train, [-1,self.input_shape])
+        self.x_train = self.x_train.astype('float32')/255
+        self.x_test = np.reshape(self.x_test, [-1,self.input_shape])
+        self.x_test = self.x_test.astype('float32')/255
 
-model = build_model()
-model.summary()
+        # network parameters
+        self.batch_size = 128
+        self.hidden_units = 256
+        self.kernel_regul = l2(0.0001)
+        self.epochs = 50
 
-if __name__ == '__main__':
-	model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-	history = model.fit(x_train,y_train, epochs=epochs, validation_data=(x_test,y_test), batch_size=batch_size)
-	print("\n")
-	print(history.history.keys())
-	
-	score = model.evaluate(x_test, y_test, batch_size=batch_size)
-	print("\nTest accuracy: %.1f%%" % (100.0*score[1]))
-	print("\n")
+class MLP(Init):
+    def build_train_model(self):
+        # build model
+        self.inputs = Input(shape=(self.input_shape,))
+        for i in range(2):
+            self.y = self.inputs
+            self.y = Dense(self.hidden_units,kernel_regularizer=self.kernel_regul)(self.y)
+            self.y = Activation('relu')(self.y)
+
+        self.y = Dense(self.num_labels)(self.y)
+        self.outputs = Activation('softmax')(self.y)
+        self.model = Model(self.inputs,self.outputs)
+
+        self.model.summary()
+
+        # train model
+        self.model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+        self.model.fit(self.x_train,self.y_train, epochs=self.epochs, validation_data=(self.x_test,self.y_test),
+                       batch_size=self.batch_size)
+        self.score = self.model.evaluate(self.x_test,self.y_test,batch_size=self.batch_size)
+        print("\nTest accuracy: %.1f%%" % (100.0*self.score[1]))
+        print("\n")
+
+
+if __name__ =='__main__':
+    mlp = MLP()
+    mlp.build_train_model()
